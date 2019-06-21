1529c6ea7d81b9119cee6c03c0b2de5a67a676b1
Ubuntu <ubuntu@ip-172-31-84-191.ec2.internal>
2019-06-21 16:47:54 +0000

    Historic file using javascript functions

---
+++ assets/js/main.js
@@ -0,0 +1,158 @@
+// // alert("here)");
+
+// var submitButton = document.getElementById("firstUserButtonId");
+// submitButton.onclick= function() {
+//     createAlert();
+//     alert("one");
+//     alert("two");
+//     alert("three");
+
+// }
+
+// function createAlert(){
+//     var inputElement = document.getElementById("firstUserValue");
+//     // var valueOfInput = inputElement.value;
+//     inputElement.value = "this is my value";
+//     inputElement.style.backgroundColor="green";
+//     firstAlert();
+// }
+
+// function firstAlert(){
+//     alert("this is text");
+// }
+
+
+
+
+// var smilingCatElement = document.getElementById("smilingCatElement");
+
+// smilingCatElement.onmouseover= function() {
+// smilingCatElement.src ="assets/images/maxresdefault.jpg";
+// smilingCatElement.style.height = "600px";
+// smilingCatElement.style.width = "600px";
+// }
+
+// // smilingCatElement.onclick= function() {
+// // smilingCatElement.src ="assets/images/maxresdefault.jpg";
+// //image does not have to be included if it is the same source!
+// // smilingCatElement.style.height = "300px";
+// // smilingCatElement.style.width = "300px";
+// // }
+
+
+// smilingCatElement.onmouseout= function() {
+// smilingCatElement.src ="assets/images/maxresdefault.jpg";
+// smilingCatElement.style.height = "200px";
+// smilingCatElement.style.width = "200px";
+// }
+
+
+
+
+// second set!--------------
+
+// window.onload= function() {
+// smilingCatElement.src ="assets/images/pexels-photo-264206.jpeg";
+// smilingCatElement.style.height = "200px";
+// smilingCatElement.style.width = "200px";
+// }
+
+// smilingCatElement.onmousedown= function() {
+// smilingCatElement.src ="assets/images/maxresdefault.jpg";
+// smilingCatElement.style.height = "200px";
+// smilingCatElement.style.width = "200px";
+// }
+
+
+// smilingCatElement.onmouseup= function() {
+// smilingCatElement.src ="assets/images/pexels-photo-264206.jpeg";
+// smilingCatElement.style.height = "200px";
+// smilingCatElement.style.width = "200px";
+// }
+
+// smilingCatElement.onfocus= function() {
+// smilingCatElement.src ="assets/images/pexels-photo-264206.jpeg";
+// smilingCatElement.style.height = "200px";
+// smilingCatElement.style.width = "200px";
+// }
+
+
+
+// new section of code---->
+
+// var buttonElement = document.getElementById("buttonId");
+// var buttonElement1 = document.getElementById("buttonId1");
+// var buttonElement2 = document.getElementById("buttonId2");
+// var divElement = document.getElementById("thisJavascript");
+
+// window.onload = function() {
+//  divElement.innerHTML = "<img id='animalpicture' src='assets/images/pexels-photo-264206.jpeg' height='200px' width='200px' alt='animal'></img>"
+ 
+//  }
+ 
+//  buttonElement.onclick =function(){
+//      divElement.innerHTML += "<p> This is a paragraph</p>";
+     
+//  }
+ 
+//  buttonElement1.onclick = function (){
+//      divElement.innerHTML ="<p> This is another paragraph</p>" + divElement.innerHTML
+     
+//  }
+
+// buttonElement2.onclick = function(){
+//     alert(divElement.innerHTML);
+// }
+ 
+ 
+ //  -------Input value challenge-------//
+ 
+//  var smilingDogElement= document.getElementById("smilingDogElement");
+//  var firstButtonClass1= document.getElementById("firstButtonClass1");
+//  var firstUserInput = document.getElementById("firstUserInput");
+ 
+ 
+// firstButtonClass1.onmousedown = function() {
+//     // alert(firstUserInput.value);
+// firstUserInput.value = "This is Dog 2";
+// firstButtonClass1.style.background="red";
+// smilingDogElement.src="assets/images/maxresdefault.jpg";
+//  smilingDogElement.style.height="200px";
+//  smilingDogElement.style.width="200px";
+// }
+
+// firstButtonClass1.onmouseup= function() {
+//  firstUserInput.value= "Dog1";
+//  firstButtonClass1.style.background="none";
+//  smilingDogElement.src="assets/images/pexels-photo-264206.jpeg";
+//  smilingDogElement.style.height="200px";
+//  smilingDogElement.style.width="200px";
+  
+// }
+
+// var launchButton= document.getElementById("launchButton");
+
+// // challenge2---------------//
+//  var myArray;
+//  myArray.push(2);
+// console.log(myArray);
+
+// for (i = 0; i < 10; i++) { 
+// 	console.log("I am happy");
+// }
+ 
+// }
+
+//  launchButton.onclick= function() {
+  
+//  }
+$("#myButton").on("click", function(){
+    alert("whats te issue");
+    $("#myPara").fadeIn(2000);
+    });
+
+
+// $("#myButton")
+
+    // alert("here");
+
